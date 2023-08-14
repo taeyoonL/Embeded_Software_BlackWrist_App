523957a2b09f157fc5873cd03c33ce0566831a50
Taeyoon Lee <132141316+taeyoonL@users.noreply.github.com>
2023-08-15 00:37:11 +0900

    Add files via upload

---
+++ DeactivateViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DeactivateViewController.swift
+//  Embeded_Software
+//
+//  Created by 이태윤 on 2023/08/14.
+//
+
+import UIKit
+
+class DeactivateViewController: UIViewController {
+
+    
+    @IBOutlet var label_1: UILabel!
+    @IBOutlet var label_2: UILabel!
+    @IBOutlet var label_3: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ StartViewController.swift
@@ -0,0 +1,36 @@
+//
+//  StartViewController.swift
+//  Embeded_Software
+//
+//  Created by 이태윤 on 2023/08/14.
+//
+
+import UIKit
+
+class StartViewController: UIViewController {
+
+    
+    @IBOutlet var label_1: UILabel!
+    @IBOutlet var label_2: UILabel!
+    @IBOutlet var label_3: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func warning_view(_ sender: UIButton) {
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
